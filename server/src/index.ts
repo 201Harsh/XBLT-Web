40320482012001f99f9168b263c145a7f8e001ad
Harsh <pandeygautam039@gmail.com>
2026-02-12 13:49:16 +0530

    Bsic Setup for Signin Logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,12 @@
-import express , { Request, Response } from "express";
+import "./config/dotenv.js";
+import http from "http";
+import app from "./app.js";
+import connectDB from "./db/db.js";
 
-const app = express();
-const PORT = 5000;
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
 
-app.use(express.json());
-
-app.get("/", (req : Request, res : Response) => {
-  res.json({
-    message: "⚡ XBLT Backend Running",
-    status: "OK",
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`⚡ Server running on http://localhost:${PORT}`);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  connectDB();
 });
